2554338f0f0a60095290ff78a800916268c08ddc
colin <aq709424295@gmail.com>
2020-08-29 23:03:00 +0800

    add 657

---
+++ LeetCode_657_RobotReturnToOrigin(easy)/src/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        boolean result=s.judgeCircle("UD");
+        System.out.println(result);
+    }
+    public boolean judgeCircle(String moves) {
+        boolean result=false;
+        int x=0;
+        int y=0;
+        for(int i=0;i<moves.length();i++){
+            switch (moves.charAt(i)){
+                case 'R':
+                    x=x+1;
+                    break;
+                case 'L':
+                    x=x-1;
+                    break;
+                case 'U':
+                    y=y+1;
+                    break;
+                case 'D':
+                    y=y-1;
+                    break;
+            }
+        }
+        return x==0 && y==0;
+    }
+}
