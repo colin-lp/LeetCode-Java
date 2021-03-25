bfa6a689f5ce73791cc9efe99c0cec6ad7302769
colin <aq709424295@gmail.com>
2021-03-25 21:27:24 +0800

    add 82(list)

---
+++ LeetCode_82_Remove Duplicates From Sorted List ii(medium)/src/Solution.java
@@ -0,0 +1,35 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode zero = new ListNode(-1);
+        zero.next = head;
+        ListNode cur = zero;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return zero.next;
+    }
+}
