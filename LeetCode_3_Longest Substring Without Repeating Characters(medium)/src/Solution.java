e05bb71c826e0663ab78a92f5c500851eb627591
colin <aq709424295@gmail.com>
2021-04-09 22:45:14 +0800

    add 3(sliding window 滑动窗口)

---
+++ LeetCode_3_Longest Substring Without Repeating Characters(medium)/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        System.out.println(s.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    Set<Character> set = new HashSet<>();
+
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0, right = 0;
+        int rst = 0;
+        while (right < s.length()) {
+            if(left!=0){
+                set.remove(s.charAt(left-1));
+            }
+            while(right<s.length() && !set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                right++;
+            }
+            left++;
+            rst=Math.max(rst,right-left+1);
+        }
+        return rst;
+    }
+}
